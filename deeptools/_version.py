5a44004af687c7a3fb5efe747cffb5c32c7e3fde
fidelram <fidel.ramirez@gmail.com>
2014-06-13 12:32:43 +0200

    Improved help message for sampleLength and numberOfSamples in bamCompare

--- deeptools/_version.py
+++ deeptools/_version.py
@@ -2,4 +2,4 @@
 # This file is originally generated from Git information by running 'setup.py
 # version'. Distribution tarballs contain a pre-generated copy of this file.
 
-__version__ = '1.5.8-15-g6f94e8f'
+__version__ = '1.5.8.1'
