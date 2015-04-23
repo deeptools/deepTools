3b95235cf264824570ce3e79dd5bd57a3273a9cf
fidelram <fidel.ramirez@gmail.com>
2015-04-23 16:34:07 +0200

    move version number to 1.6

--- deeptools/_version.py
+++ deeptools/_version.py
@@ -2,4 +2,4 @@
 # This file is originally generated from Git information by running 'setup.py
 # version'. Distribution tarballs contain a pre-generated copy of this file.
 
-__version__ = '1.5.9.1'
+__version__ = '1.6.0'
