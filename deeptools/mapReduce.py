ee2f21547d4bda59a4581094d6e538248b46d932
Fidel Ramirez <ramirez@ie-freiburg.mpg.de>
2015-12-04 12:15:41 +0100

    Fixed issue with BED intervals and region. If a bed interval ends after the user defined region is was not included. This is fixed now.

--- deeptools/mapReduce.py
+++ deeptools/mapReduce.py
@@ -51,16 +51,17 @@ def mapReduce(staticArgs, func, chromSize,
         print "genome partition size for multiprocessing: {}".format(
             genomeChunkLength)
 
-    regionStart = 0
+    region_start = 0
+    region_end = None
 
     # if a region is set, that means that the task should be only cover
     # the given genomic position
 
     if region:
-        chromSize, regionStart, regionEnd, genomeChunkLength = \
-            getUserRegion(chromSize, region)
+        chromSize, region_start, region_end, genomeChunkLength = getUserRegion(chromSize, region)
         if verbose:
-            print (chromSize, regionStart, regionEnd, genomeChunkLength)
+            print "chrom size: {}, region start: {}, region end: {}, " \
+                  "genome chunk length sent to each procesor: {}".format(chromSize, region_start, region_end, genomeChunkLength)
 
     if bedFile:
         bed_interval_tree = BED_to_interval_tree(bedFile)
@@ -70,16 +71,21 @@ def mapReduce(staticArgs, func, chromSize,
             chromSize = [x for x in chromSize if x[0] in bed_interval_tree.keys()]
             if not len(chromSize):
                 exit("*ERROR*\nChromosome names in bed file do not match the chromosome names files to process")
+
         else:
-            if chromSize[0][0] not in bed_interval_tree.keys() or \
-               len(bed_interval_tree[chromSize[0][0]].find(regionStart, regionEnd)) == 0 :
+            bed_in_region = bed_interval_tree[chromSize[0][0]].find(region_start, region_end)
+            if chromSize[0][0] not in bed_interval_tree.keys() or len(bed_in_region) == 0:
                 exit("*ERROR*\nThe specified region {} does not contain any of the "
                      "intervals in the bed file".format(region))
+            # the user region has to be extended, otherwise if a bed interval ends after the region is not counted
+            if bed_in_region[-1].end > region_end:
+                chromSize[0][1] = bed_in_region[-1].end
+
     TASKS = []
     # iterate over all chromosomes
     for chrom, size in chromSize:
         # the start is zero unless a specific region is defined
-        start = 0 if regionStart == 0 else regionStart
+        start = 0 if region_start == 0 else region_start
         for startPos in xrange(start, size, genomeChunkLength):
             endPos = min(size, startPos + genomeChunkLength)
             if self_ is not None:
@@ -96,7 +102,7 @@ def mapReduce(staticArgs, func, chromSize,
             if bedFile:
                 # this method to get the bedFile regions may seem
                 # cumbersome but I (fidel) think is better to
-                # balance the load between multiple procesors.
+                # balance the load between multiple processors.
                 # This method first partitions the genome into smaller
                 # chunks and then, for each chunk, the list of
                 # regions overlapping the chunk interval is added.
@@ -135,11 +141,17 @@ def mapReduce(staticArgs, func, chromSize,
     return res
 
 
-def getUserRegion(chromSizes, regionString, max_chunk_size=1e6):
+def getUserRegion(chrom_sizes, region_string, max_chunk_size=1e6):
     """
     Verifies if a given region argument, given by the user
-    is valid. The format of the regionString is chrom:start:end:tileSize
+    is valid. The format of the region_string is chrom:start:end:tileSize
     where start, end and tileSize are optional.
+
+    :param chrom_sizes: dictionary of chromosome/scaffold size. Key=chromosome name
+    :param region_string: a string of the form chr:start:end
+    :param max_chunk_size: upper limit for the chunk size
+    :return: tuple chrom_size for the region start, region end, chunk size
+
     >>> data = getUserRegion({'chr2': 1000}, "chr1:10:10")
     Traceback (most recent call last):
     NameError: Unkown chromosome: chr1
@@ -154,47 +166,47 @@ def getUserRegion(chromSizes, regionString, max_chunk_size=1e6):
     >>> getUserRegion({'chr2': 200000}, "chr2:10:123344:3")
     ([('chr2', 123344)], 9, 123345, 123336)
     """
-    region = regionString.split(":")
+    region = region_string.split(":")
     chrom = region[0]
-    chromSizes = dict(chromSizes)
+    chrom_sizes = dict(chrom_sizes)
 
     try:
-        chromSizes[chrom]
+        chrom_sizes[chrom]
     except KeyError:
         raise NameError("Unkown chromosome: %s\nKnown "
-                        "chromosomes are: %s " % (chrom, chromSizes.keys()))
+                        "chromosomes are: %s " % (chrom, chrom_sizes.keys()))
     try:
-        regionStart = int(region[1])
+        region_start = int(region[1])
     except IndexError:
-        regionStart = 0
+        region_start = 0
     try:
-        regionEnd = int(region[2]) if int(region[2]) <= chromSizes[chrom] \
-            else chromSizes[chrom]
+        region_end = int(region[2]) if int(region[2]) <= chrom_sizes[chrom] \
+            else chrom_sizes[chrom]
     except IndexError:
-        regionEnd = chromSizes[chrom]
-    if regionStart > regionEnd or regionStart < 0:
-        raise NameError("%s not valid. The format is chrom:start:end. "
-                        "Without comas, dashes or dots. " % (regionString))
+        region_end = chrom_sizes[chrom]
+    if region_start > region_end or region_start < 0:
+        raise NameError("{} not valid. The format is chrom:start:end. "
+                        "Without comas, dashes or dots. ".format(region_string))
     try:
         tilesize = int(region[3])
     except IndexError:
         tilesize = None
 
-    chromSizes = [(chrom, regionEnd)]
+    chrom_sizes = [(chrom, region_end)]
 
-    # if tilesize is given, make regionStart and regionEnd
+    # if tilesize is given, make region_start and region_end
     # multiple of tileSize
     if tilesize:
-        regionStart -= regionStart  % tilesize
-        regionEnd += tilesize - (regionEnd  % tilesize)
+        region_start -= region_start % tilesize
+        region_end += tilesize - (region_end % tilesize)
 
-    chunkSize = int(regionEnd - regionStart)
-    if chunkSize > max_chunk_size:
-        chunkSize = max_chunk_size
-        if tilesize and tilesize < chunkSize:
-            chunkSize -= chunkSize % tilesize
+    chunk_size = int(region_end - region_start)
+    if chunk_size > max_chunk_size:
+        chunk_size = max_chunk_size
+        if tilesize and tilesize < chunk_size:
+            chunk_size -= chunk_size % tilesize
 
-    return (chromSizes, regionStart, regionEnd, int(chunkSize))
+    return chrom_sizes, region_start, region_end, int(chunk_size)
 
 
 def BED_to_interval_tree(BED_file):
@@ -209,7 +221,8 @@ def BED_to_interval_tree(BED_file):
 
     bed_interval_tree = {}
     for line in BED_file:
-        if line[0] == "#": continue
+        if line[0] == "#":
+            continue
         fields = line.strip().split()
         chrom, start_bed, end_bed, = fields[0], int(fields[1]), int(fields[2])
 
