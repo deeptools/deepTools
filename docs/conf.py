03fac9bae1b04f51ff7b1260787e11aff692310a
Fidel Ramirez <fidel.ramirez@gmail.com>
2015-12-08 23:35:53 +0100

    added mock modules to the conf.py to allow readthedocs to compile the documentation without installing pyBigWig.

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,15 @@ import sys
 import os
 import shlex
 
+# to allow readthedocs to compile without installing some dependencies
+import mock
+
+MOCK_MODULES = ['numpy', 'scipy', 'pyBigWig']
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
