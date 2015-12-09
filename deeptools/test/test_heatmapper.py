92042eea27be8804a652089cb44969b98c6704b3
fidelram <fidel.ramirez@gmail.com>
2015-12-09 18:15:42 +0100

    change hardcoded temp filenames

--- deeptools/test/test_heatmapper.py
+++ deeptools/test/test_heatmapper.py
@@ -112,7 +112,7 @@ class TestHeatmapper(object):
         # remove xlink:href, id and url attributes
         os.system('cat {} | perl -lane \'s/xlink:href=".+?"//g; s/id=".+?"//g; s/"url\(.+?\)"//g; print $_\' > {}'.format(file1, f1))
         os.system('cat {} | perl -lane \'s/xlink:href=".+?"//g; s/id=".+?"//g; s/"url\(.+?\)"//g; print $_\' > {}'.format(file2, f2))
-        res = filecmp.cmp("/tmp/f1", "/tmp/f2")
+        res = filecmp.cmp(f1, f2)
         os.remove(f1)
         os.remove(f2)
         return res
