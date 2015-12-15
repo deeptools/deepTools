4f37df5f446d1921f1c4923ded35d2ee08f659cf
Fidel Ramirez <fidel.ramirez@gmail.com>
2015-12-16 00:16:27 +0100

    added test for multi bigwig profiles and heatmaps. Added test for overlapped lines

--- deeptools/test/test_heatmapper/make_master_images.sh
+++ deeptools/test/test_heatmapper/make_master_images.sh
@@ -5,3 +5,10 @@ plotHeatmap -m master.mat.gz --outFileName master_relabeled.svg --regionsLabel u
 plotHeatmap -m master_scale_reg.mat.gz --outFileName master_scale_reg.svg
 plotProfile -m master.mat.gz -o profile_master.svg --regionsLabel uno,dos --plotType std
 plotProfile -m master.mat.gz -o profile_master_heatmap.svg --plotType heatmap
+plotProfile -m master.mat.gz -o profile_master_overlap_lines.svg --plotType overlapped_lines
+
+# for tests with multiple bigwigs and multiple beds
+computeMatrix reference-point -R group1.bed group2.bed -S test.bw test.bw test.bw test.bw -o master_multi.mat.gz -a 100 -b 100 -bs 1
+plotHeatmap -m master_multi.mat.gz -o heatmap_master_multi_pergroup.svg --perGroup
+plotProfile -m master_multi.mat.gz -o profile_master_multi.svg --numPlotsPerRow 2 --yMax 1.5
+plotProfile -m master_multi.mat.gz -o profile_master_multi_pergroup.svg --perGroup --yMax 1.5
