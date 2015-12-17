58072ac89500ec6aca249a97b932def445b3da28
Fabian Kilpert <kilpert@ie-freiburg.mpg.de>
2015-12-17 15:47:04 +0100

    Rephrased help text

--- deeptools/bamCoverage.py
+++ deeptools/bamCoverage.py
@@ -24,11 +24,11 @@ def parseArguments():
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
             description='This tool takes an alignment of reads or fragments '
             'as input (BAM file) and generates a coverage track (bigWig or '
-            'bedGraph). '
+            'bedGraph) as output. '
             'The coverage is calculated as the number of reads for each bin, '
-            'where bins are short consecutive counting windows of a defined '
-            'size. It is possible to extended the length of the reads in this '
-            'step to better reflect the actual fragment length. bamCoverage '
+            'whereby bins are short consecutive counting windows of a defined '
+            'size. It is possible to extended the length of the reads '
+            'to better reflect the actual fragment length. *bamCoverage* '
             'offers normalization by scaling factor, Reads Per Kilobase per '
             'Million mapped reads (RPKM), and 1x depth (Reads Per Genome '
             'Coverage, RPGC).\n',
