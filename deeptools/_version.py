74562b21b205871795fbd2e4a3abddc78a5fa02b
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2015-12-18 23:20:47 +0100

    replaced wiki link references with readthedocs

--- deeptools/_version.py
+++ deeptools/_version.py
@@ -2,4 +2,4 @@
 # This file is originally generated from Git information by running 'setup.py
 # version'. Distribution tarballs contain a pre-generated copy of this file.
 
-__version__ = '1.6.0-217-g51d082d'
+__version__ = '1.6.0-505-gd122a69'
