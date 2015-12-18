62e46f9d8bee132a72dad45c53fb6b0a371b56a9
dpryan79 <dpryan79@gmail.com>
2015-12-18 14:38:04 +0100

    Set a default tmp_dir, which can be overridden with env variables

--- deeptools/config/__init__.py
+++ deeptools/config/__init__.py
@@ -2,6 +2,7 @@ import os
 import sys
 import ConfigParser
 import subprocess
+import tempfile
 
 
 def checkProgram(program, args, where_to_download):
@@ -49,6 +50,7 @@ if os.environ.get('DEEP_TOOLS_NO_CONFIG', False):
     config = ConfigParser.ConfigParser()
     config.add_section('general')
     config.set('general', 'default_proc_number', 'max/2')
+    config.set('general', 'tmp_dir', tempfile.gettempdir()) #N.B., the TMPDIR variable can be used!
 
     config.add_section('external_tools')
     config.set('external_tools', 'sort', 'sort')
