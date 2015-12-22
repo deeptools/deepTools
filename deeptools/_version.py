4f4d7dc38132275e6a78982563d072cc1c7f7f21
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2015-12-22 14:37:22 +0100

    fix merge conflict

--- deeptools/_version.py
+++ deeptools/_version.py
@@ -2,8 +2,4 @@
 # This file is originally generated from Git information by running 'setup.py
 # version'. Distribution tarballs contain a pre-generated copy of this file.
 
-
-__version__ = '1.6.0-505-gd122a69'
-
-
-
+__version__ = '1.6.0-561'
