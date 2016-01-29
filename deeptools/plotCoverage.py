ac5c2fa02e24830fc1ce9bedb3866a6a13577d45
fidelram <fidel.ramirez@gmail.com>
2016-01-29 16:28:09 +0100

    adjusted the x-axis and y-axis range of the plotCoverage image which was too large for some cases. Also added a text output with details about the coverage distribution (mean, median, std, etc).

--- deeptools/plotCoverage.py
+++ deeptools/plotCoverage.py
@@ -141,6 +141,17 @@ def main(args=None):
     sys.stderr.write("Number of non zero bins "
                      "used: {}\n".format(num_reads_per_bin.shape[0]))
 
+    if args.outRawCounts:
+        print "test"
+        # append to the generated file the
+        # labels
+        header = "#'chr'\t'start'\t'end'\t"
+        header += "'" + "'\t'".join(args.labels) + "'\n"
+        with open(args.outRawCounts.name, 'r+') as f:
+            content = f.read()
+            f.seek(0, 0)
+            f.write(header + content)
+
     if num_reads_per_bin.shape[0] < 2:
         exit("ERROR: too few non zero bins found.\n"
              "If using --region please check that this "
@@ -152,11 +163,20 @@ def main(args=None):
     fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     plt.suptitle(args.plotTitle)
     # plot up to two std from mean
+    num_reads_per_bin = num_reads_per_bin.astype(int)
     sample_mean = num_reads_per_bin.mean(axis=0)
-    std = max(num_reads_per_bin.std(axis=0))
-    y_max = max(sample_mean) + 3 * std
+    sample_std = num_reads_per_bin.std(axis=0)
+    sample_max = num_reads_per_bin.max(axis=0)
+    sample_min = num_reads_per_bin.min(axis=0)
+    sample_25 = np.percentile(num_reads_per_bin, 25, axis=0)
+    sample_50 = np.percentile(num_reads_per_bin, 50, axis=0)
+    sample_75 = np.percentile(num_reads_per_bin, 75, axis=0)
 
+    # use the largest 99th percentile from all samples to set the x_max value
+    x_max = np.max(np.percentile(num_reads_per_bin, 99, axis=0))
     # plot coverage
+    # print headers for text output
+    print("sample\tmean\tstd\tmin\t25%\t50%\t75%\tmax")
     for idx, col in enumerate(num_reads_per_bin.T):
         axs[0].plot(np.bincount(col.astype(int)).astype(float) / num_reads_per_bin.shape[0],
                     label="{}, mean={:.1f}".format(args.labels[idx], sample_mean[idx]))
@@ -164,15 +184,26 @@ def main(args=None):
         axs[1].plot(csum.astype(float)[::-1] / csum.max(),
                     label=args.labels[idx])
 
-    axs[0].set_xlim(0, y_max)
-    axs[0].set_xlabel('coverage')
-    axs[0].legend()
+        print("{}\t{:0.2f}\t{:0.2f}\t{}\t{}\t{}\t{}\t{}\t".format(args.labels[idx],
+                                                                  sample_mean[idx],
+                                                                  sample_std[idx],
+                                                                  sample_min[idx],
+                                                                  sample_25[idx],
+                                                                  sample_50[idx],
+                                                                  sample_75[idx],
+                                                                  sample_max[idx],
+                                                                  ))
+
+    axs[0].set_ylim(0, 0.02)
+    axs[0].set_xlim(0, x_max)
+    axs[0].set_xlabel('coverage (#reads per bp)')
+    axs[0].legend(fancybox=True, framealpha=0.5)
     axs[0].set_ylabel('fraction of bases sampled')
     # plot cumulative coverage
-    axs[1].set_xlim(0, y_max)
-    axs[1].set_xlabel('coverage')
+    axs[1].set_xlim(0, x_max)
+    axs[1].set_xlabel('coverage (#reads per bp)')
     axs[1].set_ylabel('fraction of bases sampled >= coverage')
-    axs[1].legend()
+    axs[1].legend(fancybox=True, framealpha=0.5)
     plt.savefig(args.plotFile.name, format=args.plotFileFormat)
 
 if __name__ == "__main__":
