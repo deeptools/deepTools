ae23a14408e29fc97d0a8635d2d9a03956c60f5a
dpryan79 <dpryan79@gmail.com>
2016-02-16 12:10:57 +0100

    Apparently the -r option for planemo doesn't exist

--- .planemo.sh
+++ .planemo.sh
@@ -13,5 +13,20 @@ sed -i '5i\    <datatype extension="deeptools_coverage_matrix" type="galaxy.data
 ./scripts/common_startup.sh --skip-venv --dev-wheels
 cd ..
 pip install . 
-/home/travis/build/fidelram/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv -r galaxy/wrapper 2>&1 | grep -v -e "^galaxy" | grep -v -e "^requests"
+/home/travis/build/fidelram/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv \
+galaxy/wrapper/bamCompare.xml \
+galaxy/wrapper/bamCoverage.xml \
+galaxy/wrapper/bamPEFragmentSize.xml \
+galaxy/wrapper/bigwigCompare.xml \
+galaxy/wrapper/computeGCBias.xml \
+galaxy/wrapper/computeMatrix.xml \
+galaxy/wrapper/correctGCBias.xml \
+galaxy/wrapper/multiBamSummary.xml \
+galaxy/wrapper/multiBigwigSummary.xml \
+galaxy/wrapper/plotCorrelation.xml \
+galaxy/wrapper/plotCoverage.xml \
+galaxy/wrapper/plotFingerprint.xml \
+galaxy/wrapper/plotHeatmap.xml \
+galaxy/wrapper/plotPCA.xml \
+galaxy/wrapper/plotProfiler.xml 2>&1 | grep -v -e "^galaxy" | grep -v -e "^requests"
 test ${PIPESTATUS[0]} -eq 0
