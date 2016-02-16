ba3c61c42e2c13e32dc0779bf51fd10b7f989679
dpryan79 <dpryan79@gmail.com>
2016-02-16 12:03:33 +0100

    Try adding the custom datatype for planemo testing, which would allow testing everything

--- .planemo.sh
+++ .planemo.sh
@@ -7,19 +7,11 @@ source activate deeptools_galaxy
 conda install -c bioconda samtools
 git clone --depth 1 https://github.com/galaxyproject/galaxy.git clone
 cd clone
+#Add the custom data types
+sed -i '4i\    <datatype extension="deeptools_compute_matrix_archive" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
+sed -i '5i\    <datatype extension="deeptools_coverage_matrix" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
 ./scripts/common_startup.sh --skip-venv --dev-wheels
 cd ..
 pip install . 
-/home/travis/build/fidelram/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv \
-galaxy/wrapper/bamCompare.xml \
-galaxy/wrapper/bamCoverage.xml \
-galaxy/wrapper/bamPEFragmentSize.xml \
-galaxy/wrapper/bigwigCompare.xml \
-galaxy/wrapper/computeGCBias.xml \
-galaxy/wrapper/computeMatrix.xml \
-galaxy/wrapper/correctGCBias.xml \
-galaxy/wrapper/multiBamSummary.xml \
-galaxy/wrapper/multiBigwigSummary.xml \
-galaxy/wrapper/plotCoverage.xml \
-galaxy/wrapper/plotFingerprint.xml 2>&1 | grep -v -e "^galaxy" | grep -v -e "^requests"
+/home/travis/build/fidelram/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv -r galaxy/wrapper 2>&1 | grep -v -e "^galaxy" | grep -v -e "^requests"
 test ${PIPESTATUS[0]} -eq 0
