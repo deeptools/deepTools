9f0181d04a3f87d8bc6bd8b7bb8fa6c03a905e19
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2016-05-03 14:03:03 +0200

    some pep8 issue :-|

--- deeptools/bamPEFragmentSize.py
+++ deeptools/bamPEFragmentSize.py
@@ -83,6 +83,7 @@ def parse_arguments():
     return parser
 
 def getFragSize(bam, args):
+
         fragment_len_dict, read_len_dict = get_read_and_fragment_length(bam, return_lengths=True,
                                                                         blackListFileName=args.blackListFileName,
                                                                         numberOfProcessors=args.numberOfProcessors,
@@ -119,14 +120,13 @@ def getFragSize(bam, args):
                                                       read_len_dict['std']))
         return fragment_len_dict
 
+
 def main(args=None):
     args = parse_arguments().parse_args(args)
 
     fraglengths = {}
-
     for bam in args.bamfiles:
         fraglengths[bam] = getFragSize(bam, args)
-
 
     if args.histogram:
         import matplotlib
@@ -151,10 +151,10 @@ def main(args=None):
                 maxVal = fraglengths[bam]['mean'] * 2
 
             plt.hist(fraglengths[bam]['lengths'], 50,
-                    range=(fraglengths[bam]['min'], maxVal),
-                    alpha=0.5, label = labels[i],
-                    log=args.logScale,
-                    normed=True)
+                     range=(fraglengths[bam]['min'], maxVal),
+                     alpha=0.5, label = labels[i],
+                     log=args.logScale,
+                     normed=True)
             i += 1
 
         plt.xlabel('Fragment Length')
