95c0ef5862dd78db4cf3d7504f21ea390e035563
dpryan79 <dpryan79@gmail.com>
2016-10-05 11:45:12 +0200

    implement #410, maxPairedFragmentLength can be directly set by --maxFragmentLength

--- deeptools/countReadsPerBin.py
+++ deeptools/countReadsPerBin.py
@@ -227,13 +227,15 @@ class CountReadsPerBin(object):
             self.maxPairedFragmentLength = 4 * self.defaultFragmentLength
         else:
             self.maxPairedFragmentLength = 1000
+        if self.maxFragmentLength > 0:
+            self.maxPairedFragmentLength = self.maxFragmentLength
 
     def run(self, allArgs=None):
         # Try to determine an optimal fraction of the genome (chunkSize) that is sent to
         # workers for analysis. If too short, too much time is spend loading the files
         # if too long, some processors end up free.
         # the following values are empirical
-
+self.maxFragmentLength
         bamFilesHandlers = [bamHandler.openBam(x) for x in self.bamFilesList]
         chromSizes, non_common = deeptools.utilities.getCommonChrNames(bamFilesHandlers, verbose=self.verbose)
 
