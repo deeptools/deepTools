fd1b17a7e57cd25ef6cd9d74ff4510e8636486f7
Devon Ryan <dpryan79@gmail.com>
2017-02-09 23:26:47 +0100

    Fix channel and remove echo statements

--- .planemo.sh
+++ .planemo.sh
@@ -2,32 +2,22 @@
 blah=`mktemp -d`
 /home/travis/build/fidelram/deepTools/foo/bin/planemo database_create galaxy
 bash miniconda.sh -b -p $blah/conda
-#/home/travis/build/fidelram/deepTools/foo/bin/planemo conda_init --conda_prefix $blah/conda
 export PATH=$blah/conda/bin:$PATH
-echo "0"
 conda create -y --name deeptools_galaxy numpy matplotlib scipy
 source activate deeptools_galaxy
-conda config --add channels bioconda conda-forge
-echo "1"
+conda config --add channels bioconda
+conda config --add channels conda-forge
 conda install -c bioconda samtools
-echo "2"
 git clone --depth 1 --single-branch --branch release_16.10 https://github.com/galaxyproject/galaxy.git clone
 cd clone
 #Add the custom data types
 sed -i '4i\    <datatype extension="deeptools_compute_matrix_archive" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
 sed -i '5i\    <datatype extension="deeptools_coverage_matrix" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
-echo "3"
 pip install PyYAML==3.11
-echo "4"
 ./scripts/common_startup.sh --skip-venv --dev-wheels
-echo "5"
 cd ..
-#conda uninstall -y sqlite
-#echo "3"
-ls
 pip install .
-echo "6"
-/home/travis/build/fidelram/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv --postgres --conda_prefix $blah/conda --conda_ensure_channels bioconda,conda-forge,linux64 \
+/home/travis/build/fidelram/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv --postgres --conda_prefix $blah/conda --conda_ensure_channels bioconda,conda-forge \
 galaxy/wrapper/bamCompare.xml \
 galaxy/wrapper/bamCoverage.xml \
 galaxy/wrapper/bamPEFragmentSize.xml \
