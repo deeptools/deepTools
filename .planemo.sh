4a2493406c21a105e8bb8940649e909374a494dd
dpryan79 <dpryan79@gmail.com>
2017-03-10 14:55:53 +0100

    Update planemo testing from develop

--- .planemo.sh
+++ .planemo.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 blah=`mktemp -d`
+conda config --add channels conda-forge
+conda config --add channels bioconda
 /home/travis/build/fidelram/deepTools/foo/bin/planemo database_create galaxy
-/home/travis/build/fidelram/deepTools/foo/bin/planemo conda_init --conda_prefix $blah/conda
-export PATH=$blah/conda/bin:$PATH
-conda create -y --name deeptools_galaxy numpy matplotlib scipy
-source activate deeptools_galaxy
-conda install -c bioconda samtools
-git clone --depth 1 https://github.com/galaxyproject/galaxy.git clone
+
+git clone --depth 1 --single-branch --branch release_16.10 https://github.com/galaxyproject/galaxy.git clone
 cd clone
+touch tool-data/twobit.loc
 #Add the custom data types
 sed -i '4i\    <datatype extension="deeptools_compute_matrix_archive" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
 sed -i '5i\    <datatype extension="deeptools_coverage_matrix" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
+sed -i '7d' lib/galaxy/dependencies/pinned-requirements.txt
 ./scripts/common_startup.sh --skip-venv --dev-wheels
 cd ..
-conda uninstall -y sqlite
-pip install . 
-/home/travis/build/fidelram/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv --postgres \
+pip install .
+#galaxy/wrapper/correctGCBias.xml \
+/home/travis/build/fidelram/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv --postgres --no_conda_auto_install --no_conda_auto_init \
 galaxy/wrapper/bamCompare.xml \
 galaxy/wrapper/bamCoverage.xml \
 galaxy/wrapper/bamPEFragmentSize.xml \
@@ -23,7 +23,6 @@ galaxy/wrapper/bigwigCompare.xml \
 galaxy/wrapper/computeGCBias.xml \
 galaxy/wrapper/computeMatrix.xml \
 galaxy/wrapper/computeMatrixOperations.xml \
-galaxy/wrapper/correctGCBias.xml \
 galaxy/wrapper/multiBamSummary.xml \
 galaxy/wrapper/multiBigwigSummary.xml \
 galaxy/wrapper/plotCorrelation.xml \
