f8de46671cc9af500e465488014a1f72040a91ba
fidelram <fidel.ramirez@gmail.com>
2017-07-12 17:48:21 +0200

    update path

--- deeptools/test/test_plotCoverage.py
+++ deeptools/test/test_plotCoverage.py
@@ -20,7 +20,7 @@ def test_plotCoverage_default():
     args = "--bamfiles {0}test1.bam {0}test2.bam --plotFile {1}" \
            " --plotFileFormat png --outRawCounts {2}".format(TEST_DATA, plotfile.name, txtfile.name).split()
     deeptools.plotCoverage.main(args)
-    assert filecmp.cmp(os.path.join(ROOT, 'outRawCounts_default.tabular'), '/tmp/_test.tab') is True
+    assert filecmp.cmp(os.path.join(ROOT, 'outRawCounts_default.tabular'), txtfile.name) is True
 
     res = compare_images(ROOT + 'plotCoverage_default.png', plotfile.name, tolerance)
     assert res is None, res
