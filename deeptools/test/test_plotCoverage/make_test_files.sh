63fbb00f2a0bbf54f2014ffd9293ef2c3cd7db0d
fidelram <fidel.ramirez@gmail.com>
2017-07-12 16:54:47 +0200

    use matplotlib testing function `compare_images` to test images instead of the svg method. Compare_images should be more stable across platforms and matplotlib versions. I updated the requirements to matplotlib 2

--- deeptools/test/test_plotCoverage/make_test_files.sh
+++ deeptools/test/test_plotCoverage/make_test_files.sh
@@ -1,1 +1,1 @@
-plotCoverage --bamfiles ../test_data/test1.bam ../test_data/test2.bam --plotFile plotCoverage_default.svg  --plotFileFormat svg --outRawCounts outRawCounts_default.tabular
+plotCoverage --bamfiles ../test_data/test1.bam ../test_data/test2.bam --plotFile plotCoverage_default.png  --plotFileFormat png --outRawCounts outRawCounts_default.tabular
