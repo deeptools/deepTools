8fe301d316798fc3aeb8ea8544554ffa610ba780
dpryan79 <dpryan79@gmail.com>
2017-07-19 13:39:11 +0200

    blah

---
+++ .planemo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+blah=`mktemp -d`
+conda config --add channels conda-forge
+conda config --add channels bioconda
+/home/travis/build/fidelram/deepTools/foo/bin/planemo database_create galaxy
+
+git clone --depth 1 --single-branch --branch release_16.10 https://github.com/galaxyproject/galaxy.git clone
+cd clone
+touch tool-data/twobit.loc
+#Add the custom data types
+sed -i '4i\    <datatype extension="deeptools_compute_matrix_archive" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
+sed -i '5i\    <datatype extension="deeptools_coverage_matrix" type="galaxy.datatypes.binary:CompressedArchive" subclass="True" display_in_upload="True"/>' config/datatypes_conf.xml.sample
+sed -i '7d' lib/galaxy/dependencies/pinned-requirements.txt
+./scripts/common_startup.sh --skip-venv --dev-wheels
+cd ..
+pip install .
+#galaxy/wrapper/correctGCBias.xml \
+/home/travis/build/fidelram/deepTools/foo/bin/planemo test --galaxy_root clone --test_data galaxy/wrapper/test-data/ --skip_venv --postgres --no_conda_auto_install --no_conda_auto_init \
+galaxy/wrapper/bamCompare.xml \
+galaxy/wrapper/bamCoverage.xml \
+galaxy/wrapper/bamPEFragmentSize.xml \
+galaxy/wrapper/bigwigCompare.xml \
+galaxy/wrapper/computeGCBias.xml \
+galaxy/wrapper/computeMatrix.xml \
+galaxy/wrapper/computeMatrixOperations.xml \
+galaxy/wrapper/multiBamSummary.xml \
+galaxy/wrapper/multiBigwigSummary.xml \
+galaxy/wrapper/plotCorrelation.xml \
+galaxy/wrapper/plotCoverage.xml \
+galaxy/wrapper/plotEnrichment.xml \
+galaxy/wrapper/plotFingerprint.xml \
+galaxy/wrapper/plotHeatmap.xml \
+galaxy/wrapper/plotPCA.xml \
+galaxy/wrapper/plotProfiler.xml 2>&1 | grep -v -e "^galaxy" | grep -v -e "^requests"
+test ${PIPESTATUS[0]} -eq 0
---
+++ setup.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import subprocess
+import re
+from distutils import sysconfig
+import glob
+
+from setuptools import setup, Extension, find_packages
+from setuptools.command.sdist import sdist as _sdist
+from setuptools.command.install import install as _install
+
+VERSION_PY = """
+# This file is originally generated from Git information by running 'setup.py
+# version'. Distribution tarballs contain a pre-generated copy of this file.
+
+__version__ = '%s'
+"""
+
+srcs = [x for x in glob.glob("deeptoolsintervals/tree/*.c")]
+
+libs = ["z"]
+additional_libs = [sysconfig.get_config_var("LIBDIR"), sysconfig.get_config_var("LIBPL")]
+
+module1 = Extension('deeptoolsintervals.tree',
+                    sources=srcs,
+                    libraries=libs,
+                    library_dirs=additional_libs,
+                    include_dirs=[sysconfig.get_config_var("INCLUDEPY")])
+
+
+def update_version_py():
+    if not os.path.isdir(".git"):
+        print("This does not appear to be a Git repository.")
+        return
+    try:
+        p = subprocess.Popen(["git", "describe",
+                              "--tags", "--always"],
+                             stdout=subprocess.PIPE)
+    except EnvironmentError:
+        print("unable to run git, leaving deeptools/_version.py alone")
+        return
+    stdout = p.communicate()[0]
+    if p.returncode != 0:
+        print("unable to run git, leaving deeptools/_version.py alone")
+        return
+    ver = stdout.decode().strip().replace("-g", "-")
+    f = open("deeptools/_version.py", "w")
+    f.write(VERSION_PY % ver)
+    f.close()
+    print("set deeptools/_version.py to '%s'" % ver)
+
+
+def get_version():
+    try:
+        f = open("deeptools/_version.py")
+    except EnvironmentError:
+        return None
+    for line in f.readlines():
+        mo = re.match("__version__ = '([^']+)'", line)
+        if mo:
+            ver = mo.group(1)
+            return ver
+    return None
+
+
+class sdist(_sdist):
+
+    def run(self):
+        update_version_py()
+        self.distribution.metadata.version = get_version()
+        return _sdist.run(self)
+
+
+class install(_install):
+
+    def run(self):
+        update_version_py()
+        self.distribution.metadata.version = get_version()
+        _install.run(self)
+        return
+        if os.environ.get('DEEP_TOOLS_NO_CONFIG', False):
+            return
+        self.config_file = self.install_platlib + \
+            "/deeptools/config/deeptools.cfg"
+
+    def checkProgramIsInstalled(self, program, args, where_to_download,
+                                affected_tools):
+        try:
+            subprocess.Popen([program, args],
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+            return True
+        except EnvironmentError:
+            # handle file not found error.
+            # the config file is installed in:
+            msg = "\n**{0} not found. This " \
+                  "program is needed for the following "\
+                  "tools to work properly:\n"\
+                  " {1}\n"\
+                  "{0} can be downloaded from here:\n " \
+                  " {2}\n".format(program, affected_tools,
+                                  where_to_download)
+            sys.stderr.write(msg)
+
+        except Exception as e:
+            sys.stderr.write("Error: {}".format(e))
+
+
+def openREADME():
+    """
+    This is only needed because README.rst is UTF-8 encoded and that won't work
+    under python3 iff sys.getfilesystemencoding() returns 'ascii'
+
+    Since open() doesn't accept an encoding in python2...
+    """
+    try:
+        f = open("README.rst", encoding="utf-8")
+    except:
+        f = open("README.rst")
+
+    foo = f.read()
+    f.close()
+    return foo
+
+
+setup(
+    name='deepTools',
+    version=get_version(),
+    author='Fidel Ramirez,  Devon P Ryan, Björn Grüning, Friederike Dündar, Sarah Diehl,'
+    ' Vivek Bhardwaj, Fabian Kilpert, Andreas S Richter, Steffen Heyne, Thomas Manke',
+    author_email='deeptools@googlegroups.com',
+    packages=find_packages(),
+    scripts=['bin/bamCompare', 'bin/bamCoverage', 'bin/multiBamSummary',
+             'bin/plotHeatmap', 'bin/plotFingerprint', 'bin/estimateScaleFactor',
+             'bin/bamPEFragmentSize', 'bin/computeMatrix', 'bin/plotProfile',
+             'bin/computeGCBias', 'bin/correctGCBias', 'bin/multiBigwigSummary',
+             'bin/bigwigCompare', 'bin/plotCoverage', 'bin/plotPCA', 'bin/plotCorrelation',
+             'bin/plotEnrichment', 'bin/deeptools', 'bin/computeMatrixOperations'],
+    include_package_data=True,
+    package_data={'': ['config/deeptools.cfg']},
+    url='http://pypi.python.org/pypi/deepTools/',
+    license='LICENSE.txt',
+    description='Useful tools for exploring deep sequencing data ',
+    long_description=openREADME(),
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'],
+    install_requires=[
+        "numpy >= 1.9.0",
+        "scipy >= 0.17.0",
+        "matplotlib >= 1.4.0",
+        "pysam >= 0.8.2",
+        "numpydoc >=0.5",
+        "pyBigWig >=0.2.1",
+        "py2bit >= 0.2.0"
+    ],
+    zip_safe=False,
+    ext_modules=[module1],
+    cmdclass={'sdist': sdist, 'install': install}
+)
