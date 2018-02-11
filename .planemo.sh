4f21192ff7856a15f15ce6ea6b338e4fa31be78f
Devon Ryan <dpryan79@gmail.com>
2018-02-11 23:48:14 +0100

    Fix computeGCBias wrapper

--- .planemo.sh
+++ .planemo.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-echo $PATH
 planemo=$HOME/miniconda/envs/planemo/bin/planemo
-owd=`pwd`
-temp_dir=`mktemp -d`
-cd $temp_dir
-conda config --add channels conda-forge
-conda config --add channels bioconda
-conda create -y --name gxtest numpy pysam
-#source activate gxtest
-#git clone --depth 1 https://github.com/galaxyproject/galaxy.git
-#cd galaxy
-#make client
-#./scripts/common_startup.sh --skip-venv --dev-wheels
-#cd ..
-## reset what's available in conda
-cd $owd
-#conda install --yes -c conda-forge numpy scipy matplotlib==2.1.0 plotly==2.0.12 cython
-#conda install --yes -c bioconda -c conda-forge pysam==0.14.0 pyBigWig py2bit
-#python setup.py install
 
 #galaxy/wrapper/correctGCBias.xml \
 $planemo test --no_dependency_resolution --install_galaxy \
