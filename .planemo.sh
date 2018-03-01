f65cbc47184ec0753f3da9511f5e0097206ff81d
Devon Ryan <dpryan79@gmail.com>
2018-03-01 23:11:19 +0100

    fix #678

--- .planemo.sh
+++ .planemo.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 planemo=$HOME/miniconda/envs/planemo/bin/planemo
 
-$planemo test --no_dependency_resolution --install_galaxy \
-galaxy/wrapper/alignmentSieve.xml \
-galaxy/wrapper/bamCompare.xml \
-galaxy/wrapper/bamCoverage.xml \
-galaxy/wrapper/bamPEFragmentSize.xml \
-galaxy/wrapper/bigwigCompare.xml \
-galaxy/wrapper/computeGCBias.xml \
-galaxy/wrapper/computeMatrix.xml \
-galaxy/wrapper/computeMatrixOperations.xml \
-galaxy/wrapper/correctGCBias.xml \
-galaxy/wrapper/estimateReadFiltering.xml \
-galaxy/wrapper/multiBamSummary.xml \
-galaxy/wrapper/multiBigwigSummary.xml \
-galaxy/wrapper/plotCorrelation.xml \
-galaxy/wrapper/plotCoverage.xml \
-galaxy/wrapper/plotEnrichment.xml \
-galaxy/wrapper/plotFingerprint.xml \
-galaxy/wrapper/plotHeatmap.xml \
-galaxy/wrapper/plotPCA.xml \
-galaxy/wrapper/plotProfiler.xml 2>&1 | grep -v -e "^galaxy" | grep -v -e "^requests"
+if [[ $1 == "1" ]] ; then
+    wrappers="galaxy/wrapper/alignmentSieve.xml \
+    galaxy/wrapper/bamCompare.xml \
+    galaxy/wrapper/bamCoverage.xml \
+    galaxy/wrapper/bamPEFragmentSize.xml \
+    galaxy/wrapper/bigwigCompare.xml \
+    galaxy/wrapper/computeGCBias.xml"
+elif [[ $1 == "2" ]] ; then
+    wrappers="galaxy/wrapper/computeMatrix.xml \
+    galaxy/wrapper/computeMatrixOperations.xml \
+    galaxy/wrapper/correctGCBias.xml \
+    galaxy/wrapper/estimateReadFiltering.xml \
+    galaxy/wrapper/multiBamSummary.xml \
+    galaxy/wrapper/multiBigwigSummary.xml"
+else
+    wrappers="galaxy/wrapper/plotCorrelation.xml \
+    galaxy/wrapper/plotCoverage.xml \
+    galaxy/wrapper/plotEnrichment.xml \
+    galaxy/wrapper/plotFingerprint.xml \
+    galaxy/wrapper/plotHeatmap.xml \
+    galaxy/wrapper/plotPCA.xml \
+    galaxy/wrapper/plotProfiler.xml"
+fi
+
+${planemo} test --no_dependency_resolution --install_galaxy ${wrappers} 2>&1 | grep -v -e "^galaxy" | grep -v -e "^requests"
 test ${PIPESTATUS[0]} -eq 0
