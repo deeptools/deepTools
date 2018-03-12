c43198ee08c64684b9dbab429b36070695072fe4
dpryan79 <dpryan79@gmail.com>
2018-03-12 12:13:50 +0100

    [ci skip] And more mocking

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import os
 import mock
 
 # MOCK_MODULES = ['numpy', 'numpy.ma', 'scipy', 'pyBigWig']
-MOCK_MODULES = ['pyBigWig', 'py2bit', 'plotly', 'plotly.offline']
+MOCK_MODULES = ['pyBigWig', 'py2bit', 'plotly', 'plotly.offline', 'plotly.graph_objs']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
