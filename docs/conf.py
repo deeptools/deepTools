c66ac5e9104638c4112f8438a92d1b4510f9e50a
dpryan79 <dpryan79@gmail.com>
2018-03-12 12:10:10 +0100

    [ci skip] plotly.offline too

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import os
 import mock
 
 # MOCK_MODULES = ['numpy', 'numpy.ma', 'scipy', 'pyBigWig']
-MOCK_MODULES = ['pyBigWig', 'py2bit', 'plotly']
+MOCK_MODULES = ['pyBigWig', 'py2bit', 'plotly', 'plotly.offline']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
