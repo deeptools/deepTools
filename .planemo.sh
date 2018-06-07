a6d63294219852f6a71fb237c9e97eb67af1389c
Devon Ryan <dpryan79@gmail.com>
2018-06-07 22:47:03 +0200

    Remove symlink before testing, planemo doesn't handle that.

--- .planemo.sh
+++ .planemo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 planemo=$HOME/miniconda/envs/planemo/bin/planemo
 
+# Some versions of planemo don't handle symlinks
+unlink galaxy/wrapper/test-data/test.bw
+cp deeptools/test/test_heatmapper/test.bw galaxy/wrapper/test-data/test.bw
+
 if [[ $1 == "1" ]] ; then
     wrappers="galaxy/wrapper/alignmentSieve.xml \
     galaxy/wrapper/bamCompare.xml \
