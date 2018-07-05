71ec8e09de45fe408f724fcd44dc337fcbaec545
JoseEspinosa <kadomu@gmail.com>
2018-07-05 14:04:09 +0200

    Separate n (number of samples) from n_bars (number of PCs for scree plot) otherwise not all samples are plot fix #734

--- deeptools/correlation.py
+++ deeptools/correlation.py
@@ -646,8 +646,9 @@ class Correlation:
             Wt = np.dot(m2, Vh.T).T
 
         if plot_filename is not None:
-            n = len(self.labels)
-            n_ev = eigenvalues.size
+            n = n_bars = len(self.labels)
+            if eigenvalues.size < n:
+                n_bars = eigenvalues.size
             markers = itertools.cycle(matplotlib.markers.MarkerStyle.filled_markers)
             if cols is not None:
                 colors = itertools.cycle(cols)
@@ -677,13 +678,13 @@ class Correlation:
                                  prop={'size': 12}, markerscale=0.9)
 
                 # Scree plot
-                ind = np.arange(n_ev)  # the x locations for the groups
+                ind = np.arange(n_bars)  # the x locations for the groups
                 width = 0.35        # the width of the bars
 
                 if mpl.__version__ >= "2.0.0":
-                    ax2.bar(2 * width + ind, eigenvalues[:n_ev], width * 2)
+                    ax2.bar(2 * width + ind, eigenvalues[:n_bars], width * 2)
                 else:
-                    ax2.bar(width + ind, eigenvalues[:n_ev], width * 2)
+                    ax2.bar(width + ind, eigenvalues[:n_bars], width * 2)
                 ax2.set_ylabel('Eigenvalue')
                 ax2.set_xlabel('Principal Component')
                 ax2.set_title('Scree plot')
