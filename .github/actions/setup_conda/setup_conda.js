317194dc02847423956deccb6892b666a52ecc79
dpryan79 <dpryan79@gmail.com>
2019-09-10 14:18:45 +0200

    ok

--- .github/actions/setup_conda/setup_conda.js
+++ .github/actions/setup_conda/setup_conda.js
@@ -2,26 +2,30 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const tc = require('@actions/tool-cache');
 
-const os = core.getInput('os', {required: true});
+async function run() {
+    //const os = core.getInput('os', {required: true});
 
-// This should all be cached!
-if(os == "linux") {
-    const URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh";
-} else {
-    const URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh";
+    // This should all be cached!
+    if(process.platform == "linux") {
+        const URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh";
+    } else {
+        const URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh";
+    }
+    const installerLocation = await tc.downloadTool(URL);
+
+    await exec.exec("bash", [installerLocation, "-b", "-p", "$HOME/miniconda"]);
+
+    //core.addPath("$HOME/miniconda/bin")
+
+    await exec.exec("$HOME/miniconda/bin/conda", ["config", "--set", "always_yes", "yes"]);
+
+    // Strictly speaking, only the galaxy tests need planemo and samtools
+    await exec.exec("$HOME/miniconda/bin/conda", ["create", "-n", "foo", "-q", "--yes", "-c", "conda-forge", "-c", "bioconda", "python=3.7", "numpy", "scipy", "matplotlib==3.1.1", "nose", "flake8", "plotly", "pysam", "pyBigWig", "py2bit", "deeptoolsintervals", "planemo", "samtools"]);
+
+    // Caching should end here
+
+    // Install deepTools
+    await exec.exec("$HOME/miniconda/envs/foo/bin/python", ["-m", "pip", "install", ".", "--no-deps", "--ignore-installed", "-vv"])
 }
-const installerLocation = await tc.downloadTool(URL);
 
-await exec.exec("bash", [installerLocation, "-b", "-p", "$HOME/miniconda"]);
-
-//core.addPath("$HOME/miniconda/bin")
-
-await exec.exec("$HOME/miniconda/bin/conda", ["config", "--set", "always_yes", "yes"]);
-
-// Strictly speaking, only the galaxy tests need planemo and samtools
-await exec.exec("$HOME/miniconda/bin/conda", ["create", "-n", "foo", "-q", "--yes", "-c", "conda-forge", "-c", "bioconda", "python=3.7", "numpy", "scipy", "matplotlib==3.1.1", "nose", "flake8", "plotly", "pysam", "pyBigWig", "py2bit", "deeptoolsintervals", "planemo", "samtools"]);
-
-// Caching should end here
-
-// Install deepTools
-await exec.exec("$HOME/miniconda/envs/foo/bin/python", ["-m", "pip", "install", ".", "--no-deps", "--ignore-installed", "-vv"])
+run();
