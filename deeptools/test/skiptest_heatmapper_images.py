2cc7977fc9db85ff17648f1b6abde09e7938ea17
WardDeb <ward@deboutte.be>
2023-08-16 16:57:30 +0200

    rollback heatmapperr imgs test until later

--- deeptools/test/skiptest_heatmapper_images.py
+++ deeptools/test/skiptest_heatmapper_images.py
@@ -23,8 +23,6 @@ def test_plotHeatmap_simple_plot():
       -R {test_path}/test.bed -o /tmp/mat.gz -bs 25
 
     """
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master.mat.gz --outFileName {}".format(ROOT, outfile.name).split()
     deeptools.plotHeatmap.main(args)
@@ -34,8 +32,6 @@ def test_plotHeatmap_simple_plot():
 
 
 def test_plotHeatmap_rename_labels():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
 
     args = "-m {}/master.mat.gz --outFileName {} --regionsLabel uno dos".format(ROOT, outfile.name).split()
@@ -46,8 +42,6 @@ def test_plotHeatmap_rename_labels():
 
 
 def test_plotHeatmap_scale_regions():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master_scale_reg.mat.gz --outFileName {}".format(ROOT, outfile.name).split()
     deeptools.plotHeatmap.main(args)
@@ -57,8 +51,6 @@ def test_plotHeatmap_scale_regions():
 
 
 def test_plotHeatmap_multi_bigwig_pergroup():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master_multi.mat.gz --perGroup --samplesLabel file1 file2 file3 file4 " \
            "--outFileName {}".format(ROOT, outfile.name).split()
@@ -69,8 +61,6 @@ def test_plotHeatmap_multi_bigwig_pergroup():
 
 
 def test_plotHeatmap_multiple_colors_muti_scales():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master_multi.mat.gz --colorList white,blue white,red --zMin 1 0 --zMax 4 5 " \
            "--outFileName {}".format(ROOT, outfile.name).split()
@@ -81,8 +71,6 @@ def test_plotHeatmap_multiple_colors_muti_scales():
 
 
 def test_plotHeatmap_multiple_colormap_no_boxes():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master_multi.mat.gz --colorMap Reds binary terrain --boxAroundHeatmaps no " \
            "--outFileName {}".format(ROOT, outfile.name).split()
@@ -93,8 +81,6 @@ def test_plotHeatmap_multiple_colormap_no_boxes():
 
 
 def test_plotHeatmap_interpolation():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/large_matrix.mat.gz --interpolation bilinear " \
            "--outFileName {}".format(ROOT, outfile.name).split()
@@ -105,8 +91,6 @@ def test_plotHeatmap_interpolation():
 
 
 def test_plotProfiler():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master.mat.gz --outFileName {} --regionsLabel uno dos " \
            "--plotType std".format(ROOT, outfile.name).split()
@@ -117,8 +101,6 @@ def test_plotProfiler():
 
 
 def test_plotProfiler_heatmap():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master.mat.gz --outFileName {} --plotType heatmap".format(ROOT, outfile.name).split()
     deeptools.plotProfile.main(args)
@@ -128,8 +110,6 @@ def test_plotProfiler_heatmap():
 
 
 def test_plotProfiler_overlapped_lines():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master.mat.gz --outFileName {} " \
            "--plotType overlapped_lines --yMin -1".format(ROOT, outfile.name).split()
@@ -140,8 +120,6 @@ def test_plotProfiler_overlapped_lines():
 
 
 def test_plotProfiler_multibigwig():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master_multi.mat.gz --outFileName {} " \
            "--numPlotsPerRow 2 --yMax 1.5".format(ROOT, outfile.name).split()
@@ -152,8 +130,6 @@ def test_plotProfiler_multibigwig():
 
 
 def test_plotProfiler_multibigwig_pergroup():
-    if skip:
-        return
     outfile = NamedTemporaryFile(suffix='.png', prefix='plotHeatmap_test_', delete=False)
     args = "-m {}/master_multi.mat.gz --outFileName {} " \
            "--perGroup --yMax 1.5".format(ROOT, outfile.name).split()
