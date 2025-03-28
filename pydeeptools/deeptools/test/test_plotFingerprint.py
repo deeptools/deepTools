35a22ce706239c2d3d307626c83bd1eeda6ed379
Juan Caballero <caballero@pc398.ie-freiburg.mpg.de>
2025-03-28 14:53:46 +0100

    method update

--- pydeeptools/deeptools/test/test_plotFingerprint.py
+++ pydeeptools/deeptools/test/test_plotFingerprint.py
@@ -14,7 +14,7 @@ tolerance = 13
 def test_plotFingerprint_default():
     plotfile = NamedTemporaryFile(suffix='.png', prefix='deeptools_testfile_', delete=False)
     args = "-b {0}test1.bam {0}test2.bam -o {1} --plotFileFormat png".format(TEST_DATA, plotfile.name).split()
-    deeptools.plotProfile.main(args)
+    deeptools.plotFingerprint.main(args)
 
     res = compare_images(ROOT + 'test_plotFingerprint_default.png', plotfile.name, tolerance)
     
