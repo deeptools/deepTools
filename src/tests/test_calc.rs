ced6cf2899066b8c6efa293ac89cbc55dea9f16f
WardDeb <ward@deboutte.be>
2026-07-29 09:56:47 +0200

    fix cargo test for pop std

--- src/tests/test_calc.rs
+++ src/tests/test_calc.rs
@@ -1,12 +1,12 @@
 use crate::calc::*;
 use ndarray::{Array1, Array2};
 
-mod vector_calculations_tests{
+mod vector_calculations_tests {
     use super::*;
 
     #[test]
     fn test_median() {
-        let v: Vec<u32> = vec![1,2,3,4,5];
+        let v: Vec<u32> = vec![1, 2, 3, 4, 5];
         assert_eq!(median(v), 3.0);
     }
 
@@ -21,7 +21,6 @@ mod vector_calculations_tests{
         let v: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0];
         assert_eq!(median_float(&v.iter().collect::<Vec<_>>()), 3.0);
     }
-
 
     #[test]
     fn test_min_float() {
@@ -44,7 +43,7 @@ mod vector_calculations_tests{
     #[test]
     fn test_std_float() {
         let v: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0];
-        assert_eq!(std_float(&v.iter().collect::<Vec<_>>()), 1.5811388);
+        assert_eq!(std_float(&v.iter().collect::<Vec<_>>()), 1.4142135);
 
         let v: Vec<f32> = vec![1.0, 1.0, 1.0];
         assert_eq!(std_float(&v.iter().collect::<Vec<_>>()), 0.0);
@@ -53,7 +52,7 @@ mod vector_calculations_tests{
     }
 }
 
-mod scalefactor_calculations_tests{
+mod scalefactor_calculations_tests {
     use super::*;
     #[test]
     fn test_calc_ratio() {
@@ -71,30 +70,10 @@ mod scalefactor_calculations_tests{
         let reciprocal_ratio = "reciprocal_ratio";
         let subtract = "subtract";
 
-        let r1 = calc_ratio(
-            cov1, cov2,
-            &sf1, &sf2,
-            &pc1, &pc2,
-            log2
-        );
-        let r2 = calc_ratio(
-            cov1, cov2,
-            &sf1, &sf2,
-            &pc1, &pc2,
-            ratio
-        );
-        let r3 = calc_ratio(
-            cov1, cov2,
-            &sf1, &sf2,
-            &pc1, &pc2,
-            reciprocal_ratio
-        );
-        let r4 = calc_ratio(
-            cov1, cov2,
-            &sf1, &sf2,
-            &pc1, &pc2,
-            subtract
-        );
+        let r1 = calc_ratio(cov1, cov2, &sf1, &sf2, &pc1, &pc2, log2);
+        let r2 = calc_ratio(cov1, cov2, &sf1, &sf2, &pc1, &pc2, ratio);
+        let r3 = calc_ratio(cov1, cov2, &sf1, &sf2, &pc1, &pc2, reciprocal_ratio);
+        let r4 = calc_ratio(cov1, cov2, &sf1, &sf2, &pc1, &pc2, subtract);
         assert_eq!(r1, -1.87);
         assert_eq!(r2, 0.27);
         assert_eq!(r3, -0.27);
@@ -103,11 +82,9 @@ mod scalefactor_calculations_tests{
 
     #[test]
     fn test_deseq_scalefactors() {
-        let counts = Array2::from_shape_vec((3, 3), vec![
-            1.0, 2.0, 3.0,
-            4.0, 5.0, 6.0,
-            7.0, 8.0, 9.0
-        ]).unwrap();
+        let counts =
+            Array2::from_shape_vec((3, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0])
+                .unwrap();
         let sf = deseq_scalefactors(&counts);
         let expected_sf = Array1::from_vec(vec![1.233106, 0.9864848, 0.82207066]);
         assert_eq!(sf, expected_sf);
